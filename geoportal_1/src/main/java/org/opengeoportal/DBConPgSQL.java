865aa428548c3133e3de77fc0c9acb4feb8afcf1
Ahmed Kotb <A7madKotb@gmail.com>
2018-08-28 10:44:51 +0200

    added style and coloring for all inner layers

--- geoportal_1/src/main/java/org/opengeoportal/DBConPgSQL.java
+++ geoportal_1/src/main/java/org/opengeoportal/DBConPgSQL.java
@@ -28,7 +28,8 @@ public class DBConPgSQL {
 		Connection con = null;
 		try {
 			Class.forName(dbDriverName);
-			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName, dbUserName, dbPassword);
+			//con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName, dbUserName, dbPassword);
+			con = DriverManager.getConnection("jdbc:postgresql://192.168.2.236:5432/" + dbName, dbUserName, dbPassword);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
